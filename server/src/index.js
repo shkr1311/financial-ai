39bcf6ae959bb21529a8421af33851243b3d9c23
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-08-27 18:30:13 +0530

    update CORS origin to production URL for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://financial-ai-rvju.vercel.app',
+    // origin: 'http://localhost:3000',
     credentials: true,
   })
 );
