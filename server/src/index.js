84c850843b0d85ebd780ee9c093ad9bf770fedc7
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-09-25 14:59:31 +0530

    update custom origin cors

--- server/src/index.js
+++ server/src/index.js
@@ -18,8 +18,9 @@ app.use(express.json());
 app.use(
   cors({
     // origin: 'https://financial-ai-rvju.vercel.app',
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
     // origin: 'https://financial-ai-apnn.vercel.app',
+    origin: 'https://financial-ai-07.vercel.app/',
     credentials: true,
   })
 );
